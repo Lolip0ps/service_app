d769855e47b8196781090375e07d2016d140e981
Lolip0ps <lolipops-1@mail.ru>
2023-09-12 00:46:32 +0300

    День 4 - Урок 4

--- service/servises/serializers.py
+++ service/servises/serializers.py
@@ -15,10 +15,8 @@ class SubscriptionsSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source='client.user.email')
     price = serializers.SerializerMethodField()
 
-
     def get_price(self, instance):
-        return (instance.service.full_price -
-                instance.service.full_price * (instance.plan.discount_percent / 100))
+        return instance.price
 
     class Meta:
         model = Subscriptions
